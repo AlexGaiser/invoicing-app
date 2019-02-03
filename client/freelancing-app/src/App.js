a6052e708d3abe1609737673a11a0eb49e4d88e5
AlexGaiser <alexcgaiser@gmail.com>
2019-02-03 15:32:07 -0500

    adds start stop and reset buttons to main page

--- client/freelancing-app/src/App.js
+++ client/freelancing-app/src/App.js
@@ -30,6 +30,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <Stopwatch/>
+
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
--- client/freelancing-app/src/Main/Stopwatch.js
+++ client/freelancing-app/src/Main/Stopwatch.js
@@ -3,19 +3,25 @@ import React, { Component } from 'react';
 class Stopwatch extends Component {
     constructor(props) {
         super(props);
-        this.state = { time:'' }
+        this.state = { 
+            time:'',
+            startTime:
+        }
     }
     setTime = ()=> this.setState({time:Date()})
     componentDidMount =()=>{
         setInterval(this.setTime, 500)
     }
-
+    
     render() { 
-        return ( 
+        return (
             <div>
                 <h1>{this.state.time}</h1>
+                <button>Start</button>
+                <button>Stop</button>
+                <button>Reset</button>
             </div>
-         );
+         )
     }
 }
  
