65b27cf29f9d84d90173515aaf44b3a095f68b15
addoasa <addoasa94@gmail.com>
2019-02-05 16:31:13 -0500

    Add more styling to the User page

--- client/freelancing-app/src/userpg/MainInvoice.js
+++ client/freelancing-app/src/userpg/MainInvoice.js
@@ -26,10 +26,19 @@ class MainInvoice extends React.Component{
             <div className="invoice-container">
                 <div>
                     {/* <h1 className="invoice-title">Google Logo Redesign</h1> */}
-                    <h3 className="invoice-number">{this.state.isLoaded && this.state.information[0].title}</h3>
-                    <h3 className="invoice-number">Invoice ID: {this.state.isLoaded && this.state.information[0].id}</h3>
-                    <h3 className="invoice-number">{this.state.isLoaded && this.state.information[0].date}</h3>
-                    <h3 className="invoice-number">{this.state.isLoaded && this.state.information[0].description}</h3>
+                    <div className="main-heading">
+                        <h3 className= "main-title main-details" >{this.state.isLoaded && this.state.information[0].title}</h3>
+                        <h3 className= "main-id main-details" >Invoice ID: {this.state.isLoaded && this.state.information[0].id}</h3>
+                    </div>
+                    <div className="main-line-1"></div>
+                    <h3 className= "main-date main-details" >{this.state.isLoaded && this.state.information[0].date}</h3>
+                    <h3 className= "main-description main-details" >{this.state.isLoaded && this.state.information[0].description}</h3>
+                    <h3 className= "main-description main-details" >Details:{this.state.isLoaded && this.state.information[0].extra_details}</h3>
+                    <h3 className= "main-description main-details" >Time Logged:{this.state.isLoaded && this.state.information[0].logged_time}</h3>
+                    <div className="main-line-1"></div>
+                    <h3 className= "main-description main-details" >Total Amount:{this.state.isLoaded && this.state.information[0].total_amount}</h3>
+
+                    
                 </div>
             </div>
         )
