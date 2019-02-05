162c56e49733361e6999085781259cbf4870268e
addoasa <addoasa94@gmail.com>
2019-02-05 12:23:16 -0500

    Cleaned the user page

--- client/freelancing-app/src/userpg/ListContainer.js
+++ client/freelancing-app/src/userpg/ListContainer.js
@@ -1,27 +1,11 @@
 import React from "react"
 import Axios from 'axios'
 import ListItem from './ListItem'
+import '../App.css'
 
 
 
-// class ListContainer extends React.Component{
-//     constructor(){
-//         super()
-//     }
 
-    
-   
-
-
-//     render(){
-    
-//         return( 
-//         <div className="list-container">
-            
-//         </div>
-//    ) }
-// }
-// d
 const ListContainer = (props) => {
     return(
         <div className="list-container">
