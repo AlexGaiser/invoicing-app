18be4549416ecb03457132009695ca51f52a03bf
AlexGaiser <alexcgaiser@gmail.com>
2019-02-06 14:30:43 -0500

    login info

--- client/freelancing-app/src/Login/Login.js
+++ client/freelancing-app/src/Login/Login.js
@@ -2,23 +2,65 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import {Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import {Navbar, Nav, NavDropdown, Form, FormControl, ListGroup } from 'react-bootstrap';
 
 import './Login-styles.css'
+import Axios from 'axios';
 
 
-const LoginPanel = (props) => {
+class LoginPanel extends Component {
+  constructor(props) {
+    super()
+    this.state={
 
-  return (
-    <React.Fragment>
-      <Form onSubmit={()=>alert('form submitted')}inline className="">
-        <Form.Control type="text" placeholder="Username" className="mr-sm-2" />
-        <Form.Control type="text" placeholder="Password" className="mr-sm-2" />
-        <Button type="submit" className="" variant="outline-success">LOGIN</Button>
-      </Form>
-    </React.Fragment>
-  )
+    }
+  }
+  createAuthHeader = ()=>{
+    const token = localStorage.getItem('token')
+    return {
+      headers: {
+        'Authorization': "bearer " + token
+      }
+    };
+  }
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
 
+ 
+  sendLoginInfo= async(event) =>{
+      event.preventDefault()
+      // const header = this.createAuthHeader()
+      const credentials = {username:this.state.username, password: this.state.password}
+      const response = await Axios.post('/login', credentials)
+      if(response.data.token){
+        alert('logged in ')
+        localStorage.setItem('token', response.data.token)
+      }
+      else{
+        alert('invalid username or password')
+        localStorage.setItem('token', 'null')
+      }
+      
+      
+      console.log(credentials)
+      console.log(response);
+    }
+
+
+  render(){
+    return (
+      <React.Fragment>
+        <Form onSubmit={this.sendLoginInfo}inline className="">
+          <Form.Control name="username" type="text" placeholder="Username" className="mr-sm-2" onChange={this.handleChange}/>
+          <Form.Control name="password" type="text" placeholder="Password" className="mr-sm-2" onChange={this.handleChange}/>
+          <Button type="submit" className="" variant="outline-success">LOGIN</Button>
+        </Form>
+      </React.Fragment>
+    )
+  }
+  
 }
 
 export default LoginPanel;
