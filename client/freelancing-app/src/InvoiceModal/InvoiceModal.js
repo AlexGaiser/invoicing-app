a07529bd753203f6f5287ef4ddea7fe0a6de8ca5
addoasa <addoasa94@gmail.com>
2019-02-06 19:29:10 -0500

    Fixes issue with edit and delete buttons clipping. Begun work on modal and pdf.

--- client/freelancing-app/src/InvoiceModal/InvoiceModal.js
+++ client/freelancing-app/src/InvoiceModal/InvoiceModal.js
@@ -4,7 +4,7 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import React, { Component } from 'react';
-
+import './InvoiceModal.css'
 
 
 class InvoiceModalOne extends Component {
