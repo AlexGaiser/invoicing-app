58cc03426fa6bdf2e0d81b2d410d391e730abf6f
addoasa <addoasa94@gmail.com>
2019-02-07 11:03:02 -0500

    Typography for main invoice

--- client/freelancing-app/src/userpg/MainInvoice.js
+++ client/freelancing-app/src/userpg/MainInvoice.js
@@ -29,14 +29,19 @@ const MainInvoice=(props)=> {
                 <div>
                     {/* <h1 className="invoice-title">Google Logo Redesign</h1> */}
                     <div className="main-heading">
-                        <h3 className= "main-title main-details" >{props.isLoaded && props.invoiceDisplayed.title}</h3>
-                        <h3 className= "main-id main-details" >Invoice ID: {props.isLoaded && props.invoiceDisplayed.id}</h3>
+                        <h3 className= "main-title" >{props.isLoaded && props.invoiceDisplayed.title}</h3>
+                        <h3 className= "main-id" >Invoice ID: {props.isLoaded && props.invoiceDisplayed.id}</h3>
                     </div>
                     <div className="main-line-1"></div>
                     <h3 className= "main-date" >{props.isLoaded && props.invoiceDisplayed.date}</h3>
-                    <h3 className= "main-description main-details" >Description: {props.isLoaded && props.invoiceDisplayed.description}</h3>
-                    <h3 className= "main-details" >Details:  {props.isLoaded && props.invoiceDisplayed.extra_details}</h3>
-                    <h3 className= "main-time" >Time Logged: {props.isLoaded && props.invoiceDisplayed.logged_time}</h3>
+                    <h3 className= "main-description" ><span className= "desc-word">Description:</span> {props.isLoaded && props.invoiceDisplayed.description}</h3>
+                    <h3 className= "main-details" ><span className="details-word">Details:</span>  {props.isLoaded && props.invoiceDisplayed.extra_details}</h3>
+                    <div className="main-line-1"></div>
+                    <div className="credentials-container">
+                        <h3 className= "main-time" >Time Logged: {props.isLoaded && props.invoiceDisplayed.logged_time}</h3>
+                        <h3 className= "main-time" >Time Logged: {props.isLoaded && props.invoiceDisplayed.logged_time}</h3>
+                        <h3 className= "main-time" >Time Logged: {props.isLoaded && props.invoiceDisplayed.logged_time}</h3>
+                    </div>
                     <div className="main-line-1"></div>
                     <h3 className= "main-total" >Total Amount:{props.isLoaded && props.invoiceDisplayed.total_amount}</h3>
 
